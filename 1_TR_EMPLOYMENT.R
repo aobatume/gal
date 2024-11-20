f435f1586a26722ecf11f09f677cf03df0f8c9ab
antia.campos@gmail.com <antia.campos@gmail.com>
2024-11-20 13:03:05 +0100

    AOfiltered data and TR

--- 1_TR_EMPLOYMENT.R
+++
@@ -1,5 +0,0 @@
-
-library(readr)
-employment_raw <- read_csv("prep/TR/Empregoturismo_AFILIACIONS_Mes.csv")
-View(Empregoturismo_AFILIACIONS_Mes)
-
--- prep/TR/1_TR_createlayer.R
+++ prep/TR/1_TR_createlayer.R
@@ -3,6 +3,7 @@ library(dplyr)
 setwd("prep/TR/")
 tourism_data <- read.csv("Empregoturismo_AFILIACIONS_Mes.csv")
 
+#1 PREPARE EMPLOYMENT DATA LAYER
 
 #1.1: Define tourism-related categories
 tourism_categories <- c(
@@ -37,7 +38,6 @@ province_rgn_id <- data.frame(
   rgn_id = c(1, 2, 3, 4)
 )
 
-
 # 1.6 Merge the result with province_rgn_id to include rgn_id
 result_with_ids <- result %>%
   left_join(province_rgn_id, by = "PROVINCIA") %>%
@@ -47,7 +47,41 @@ tr_jobs_pct_tourism <- complete_data %>%
   left_join(result_with_ids, by = c("rgn_id", "year")) %>%
   mutate(Ep = ifelse(is.na(Ep), NA, Ep))
 
-write.csv(tr_jobs_pct_tourism, "/Users/batume/Documents/R/OHI_GAL/region/layers/tr_jobs_pct_tourism.csv", row.names = FALSE)
+#Remove data from 2023 because the data is not complete: 
 
+tr_jobs_pct_tourism_filtered <- tr_jobs_pct_tourism %>%
+  filter(year != 2024)
+write.csv(tr_jobs_pct_tourism_filtered, "/Users/batume/Documents/R/GAL_git/region/layers/tr_jobs_pct_tourism.csv", row.names = FALSE)
 
+#2 PREPARE SUSTENTABILITY INDEX LAYER - https://www.weforum.org/publications/travel-tourism-development-index-2024/
+# 2.1: Read the raw data
+ttdi_file <- "WEF_TTDI_2021_data_for_download.xlsx"
+ttdi_raw <- read_excel(ttdi_file, skip = 2)
 
+# 2.2: Move up column names from the first row while keeping the full country names as columns too
+names(ttdi_raw)[1:9] <- as.character(ttdi_raw[1, 1:9])
+
+# 2.3 Filter for sustainability scores, select needed columns, and pivot to tidy format
+ttdi <- ttdi_raw %>%
+  filter(Title == "T&T Sustainability subindex, 1-7 (best)",
+         Attribute == "Score") %>%
+  select(year = Edition, Albania:Zambia) %>%
+  pivot_longer(cols = Albania:Zambia, names_to = "country", values_to = "score") %>%
+  mutate(score = as.numeric(score))
+
+# 2.4: Filter only for Spain
+ttdi_spain <- ttdi %>%
+  filter(country == "Spain") %>%
+  select(year, country, score)
+
+#2.5: Add the 2024 value:
+
+spain_2024 <- data.frame(
+  year = 2024,
+  country = "Spain",
+  score = 5.18
+)
+
+ttdi_spain <- rbind(ttdi_spain, spain_2024)
+
+write.csv(ttdi_spain, "wef_ttdi_spain.csv", row.names = FALSE)
